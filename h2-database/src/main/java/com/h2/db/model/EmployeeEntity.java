1628c24b0950e73793ee16762b062609aff39b10
Mehul Kumar <100113904+urstarmehul@users.noreply.github.com>
2022-03-06 23:24:24 +0530

    Project Uploaded

---
+++ h2-database/src/main/java/com/h2/db/model/EmployeeEntity.java
@@ -0,0 +1,93 @@
+package com.h2.db.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_EMPLOYEES")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="first_name")
+    private String firstName;
+    
+    @Column(name="last_name")
+    private String lastName;
+    
+    @Column(name="email", nullable=false, length=200)
+    private String email;
+    
+    @Column(name="salary")
+    private int salary;
+    
+    @Column(name="designation")
+    private String designation;
+    
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", salary=" + salary + ", designation=" + designation + "]";
+	}
+
+   /* @Override
+    public String toString() {
+        return "EmployeeEntity [id=" + id + ", firstName=" + firstName + 
+                ", lastName=" + lastName + ", email=" + email   + "]";
+    }  */
+	
+}
---
+++ h2-database/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email, salary, designation) 
+VALUES
+  	('Mehul', 'Kumar', 'kmrmehul16@gmail.com', 20000, 'ASE'),
+  	('Shivam', 'Singh', 'shivam1234@gmail.com', 25000, 'ATE'),
+  	('Rahul', 'Gupta', 'rahulkumar321@gmail.com', 30000, 'HR');
+  	
